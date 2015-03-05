60d7480777a22d578b568dbda468d328d6630e61
Kane Blueriver <kxxoling@gmail.com>
2015-03-05 18:35:40 +0800

    docker config

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def get_user_id_by_sso_user_id(sso_user_id):
 
 if __name__ == '__main__':
     app.secret_key = "You won't guess this"
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
